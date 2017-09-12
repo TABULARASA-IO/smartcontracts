5d8ca5c082b97721e7e648ec87de1516361316bf
sevenflash <sevenflash@yandex.ru>
2017-09-12 12:03:10 +0300

    migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,2 +1,10 @@
+const Token = artifacts.require('Token');
+const Crowdsale = artifacts.require('ICO');
+const ECRecovery = artifacts.require('ECRecovery');
+const TokenHolder = artifacts.require('TokenHolder');
+const TokenHolderFactory = artifacts.require('TokenHolderFactory');
+
 module.exports = function(deployer) {
+	deployer.deploy(ECRecovery);
+	deployer.link(ECRecovery, [Token, TokenHolder, TokenHolderFactory]);
 };
