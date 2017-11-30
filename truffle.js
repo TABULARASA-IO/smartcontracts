79104ab7502fa55ebaa3f0e1d69244c7078ca280
sevenflash <sevenflash@yandex.ru>
2017-11-30 21:47:25 +0300

    ...ready for migration!

--- truffle.js
+++ truffle.js
@@ -1,10 +1,28 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+
+let infuraToken = "";
+let mnemonic = "";
+
+if(fs.existsSync("mnemonic")) {
+  mnemonic = fs.readFileSync("mnemonic");
+}
+
 module.exports = {
   networks: {
+    mainnet: {
+        provider: new HDWalletProvider(mnemonic, "https://mainnet.infura.io/"+infuraToken),
+        network_id: 1
+    },
+    ropsten: {
+        provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infuraToken),
+        network_id: 3
+    },
     development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*", // Match any network id
-      gasPrice: 1
+        host: "localhost",
+        port: 8545,
+        network_id: "*", // Match any network id
+        gasPrice: 1
     }
   }
 };
